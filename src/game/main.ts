dd3369dc926bf1fe2f8933def6fee63d89d8c7ac
citrono-lemon <citrono-lemon@gmail.com>
2022-01-24 17:10:45 +0900

    First Commit (Under Development)

---
+++ src/game/main.ts
@@ -0,0 +1,45 @@
+import 'phaser'
+
+/**
+ * メインシーン
+ * 一応説明しておくと、
+ * init⇒preload⇒create⇒update⇒update⇒...
+ * のようなライフサイクルで動作する
+ */
+class MainScene extends Phaser.Scene {
+  constructor() {
+    super({
+      key: 'Main',
+    })
+  }
+
+  /**
+   * 初期処理
+   */
+  init(): void {
+    console.log("init")
+  }
+
+  /**
+   * アセットデータ読込などを行う処理
+   */
+  preload(): void {
+    console.log("preload")
+  }
+
+  /**
+   * ゲーム画面の作成処理やイベントアクションを記述する処理
+   */
+  create(): void {
+    console.log("create")
+    this.add.text(10, 10, "Hello, phaser")
+  }
+
+  /**
+   * メインループ
+   */
+  update(): void {
+  }
+}
+
+export default MainScene
